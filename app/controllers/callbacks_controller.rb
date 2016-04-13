46c9fe5e55b305f68bb3dc1155d9f9a5e292600c
DumDumGeniuss <dumdumgenius@gmail.com>
2016-04-13 11:21:10 +0800

    Remove Rescue in Callback Ctrl

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,10 +1,7 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def provider
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    begin
-      sign_in_and_redirect @user
-    rescue
-    end
+    sign_in_and_redirect @user
   end
 
   alias_method :facebook, :provider
